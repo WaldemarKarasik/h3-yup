30014eaeb46069eb598d86a500015d306e91d866
WaldemarKarasik <waldemarkarasik@gmail.com>
2023-07-03 13:31:49 +0800

    create composable for query validation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as useValidatedBody } from "./useValidatedBody";
 export { default as sendH3Error } from "./sendH3Error";
+export { default as useValidatedQuery } from "./useValidatedQuery";
